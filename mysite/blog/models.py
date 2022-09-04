82ece5b2308691f3974d43be194ae7feb3797183
vishal <vishal@gmail.com>
2022-09-05 00:29:15 +0530

    Changed the HTML for the site.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-# from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Post(models.Model):
-    auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    auther = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
