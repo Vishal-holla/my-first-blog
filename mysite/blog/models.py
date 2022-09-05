944c75103af6ed1db38fc242d4b631bb13ed59a8
vishal <vishal@gmail.com>
2022-09-05 12:21:42 +0530

    publish_date ko published_date me change kiye or git ka file delete ho gya tha

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -6,15 +6,15 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    auther = models.ForeignKey(
+    author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    publish_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publish_date = timezone.now
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
